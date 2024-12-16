30456fa551fbe7c754981717a97395d9fe7ae332
code Blooded <108016919+Ahmed-Naseer-01@users.noreply.github.com>
2024-12-16 11:42:35 +0500

    Initial commit

---
+++ First-Spring-app/myspringapp/src/main/java/com/spring_first_project/myspringapp/rest/FirstRestController.java
@@ -0,0 +1,19 @@
+package com.spring_first_project.myspringapp.rest;
+
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FirstRestController {
+
+//    injecting property in the rest controller ( this property is define in the application.property file)
+    @Value("${coach.name}")
+    private String coachName;
+
+    @GetMapping("/")
+    public String message() { return "hello from Rest Controller"; }
+    @GetMapping("/page-01")
+    public String message1() { return "hello from page 01"; }
+}
