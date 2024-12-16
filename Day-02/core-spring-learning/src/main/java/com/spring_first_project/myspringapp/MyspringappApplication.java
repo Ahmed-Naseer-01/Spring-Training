fe08742603c5c6d4d8bca02ed3afa20f455f2ff8
code Blooded <108016919+Ahmed-Naseer-01@users.noreply.github.com>
2024-12-16 12:06:44 +0500

    added new directory

---
+++ Day-02/core-spring-learning/src/main/java/com/spring_first_project/myspringapp/MyspringappApplication.java
@@ -0,0 +1,13 @@
+package com.spring_first_project.myspringapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyspringappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyspringappApplication.class, args);
+	}
+
+}
---
+++ Day-02/core-spring-learning/src/test/java/com/spring_first_project/myspringapp/MyspringappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring_first_project.myspringapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyspringappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
