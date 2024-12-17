b82cbfbb7bc2429e07098c8187b5c2091d064f5a
code Blooded <108016919+Ahmed-Naseer-01@users.noreply.github.com>
2024-12-17 14:37:31 +0500

    add : create CoachController

---
+++ Day-03/myspringapp/src/main/java/com/spring_first_project/myspringapp/controller/CoachController.java
@@ -0,0 +1,21 @@
+package com.spring_first_project.myspringapp.controller;
+import com.spring_first_project.myspringapp.entity.Coach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+@RestController
+public class CoachController {
+
+     private final Coach newcoach;
+    @Autowired
+     public CoachController(@Qualifier("cricketCoach")Coach theNewcoach) {
+        this.newcoach = theNewcoach;
+        System.out.println("coachController called " + this.getClass().getName());
+    }
+    @GetMapping("/dailyworkout")
+    public String getDailyWorkout() { return newcoach.getDailyWorkout(); }
+}
