f63f6e7bfd0f1d04a19578c2ee9dad4f64690870
code Blooded <108016919+Ahmed-Naseer-01@users.noreply.github.com>
2024-12-17 14:39:57 +0500

    add : PostConstruct & PreDestroy methods

--- Day-03/myspringapp/src/main/java/com/spring_first_project/myspringapp/entity/CricketCoach.java
+++ Day-03/myspringapp/src/main/java/com/spring_first_project/myspringapp/entity/CricketCoach.java
@@ -1,5 +1,7 @@
 package com.spring_first_project.myspringapp.entity;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -11,4 +13,12 @@ public class CricketCoach implements Coach {
     public String getDailyWorkout() {
         return "Practical cricket workout";
     }
+    @PostConstruct
+    public void doMyStartupStuff() {
+        System.out.println("Cricket Coach doMyStartupStuff called");
+    }
+    @PreDestroy
+    public void myEndUpStuff() {
+        System.out.println("Cricket Coach myEndUpStuff called");
+}
 }
