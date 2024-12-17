2668a3ae12e2d3485304ad0b9943ea573aa8672f
code Blooded <108016919+Ahmed-Naseer-01@users.noreply.github.com>
2024-12-17 12:25:12 +0500

    initial commit

--- Day-03/myspringapp/src/main/java/com/spring_first_project/myspringapp/MyspringappApplication.java
+++ Day-03/myspringapp/src/main/java/com/spring_first_project/myspringapp/MyspringappApplication.java
@@ -3,13 +3,6 @@ package com.spring_first_project.myspringapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
-/*
-@SpringBootApplication (scanBasePackages = {"here we will define base package",
-		"here we will define the package that's outside the our base package"})
- */
-
-
 @SpringBootApplication
 public class MyspringappApplication {
 
