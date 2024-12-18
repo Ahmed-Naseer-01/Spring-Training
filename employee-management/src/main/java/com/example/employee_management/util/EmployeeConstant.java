b8ca360afad18eb09c299059c781f75f27c58be4
code Blooded <108016919+Ahmed-Naseer-01@users.noreply.github.com>
2024-12-18 16:48:39 +0500

    add : create constant entity

---
+++ employee-management/src/main/java/com/example/employee_management/util/EmployeeConstant.java
@@ -0,0 +1,12 @@
+package com.example.employee_management.util;
+
+public class EmployeeConstant {
+    static final String ROOT = "root";
+    static final String PASS = "123ahmed";
+    static final String DATE_FORMAT = "dd-MM-yyyy";
+    // Enum to define address types
+    public enum AddressType {
+        TEMPORARY,
+        PERMANENT
+    }
+}
