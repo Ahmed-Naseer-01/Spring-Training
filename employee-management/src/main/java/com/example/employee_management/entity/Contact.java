57e8a2b9e95b9558e1a4da48e58dcdb55e770c90
code Blooded <108016919+Ahmed-Naseer-01@users.noreply.github.com>
2024-12-18 16:47:34 +0500

    add : added contact entity

---
+++ employee-management/src/main/java/com/example/employee_management/entity/Contact.java
@@ -0,0 +1,33 @@
+package com.example.employee_management.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Contact {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    String phoneNumber;
+    String email;
+
+    @ManyToOne
+    @JoinColumn(name = "employee_id")
+    private Employee employee;
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
