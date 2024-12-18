386f1ec835eb591916f7f256f93ef81b5a1a70b7
code Blooded <108016919+Ahmed-Naseer-01@users.noreply.github.com>
2024-12-18 16:50:13 +0500

    add : create employee mapper

---
+++ employee-management/src/main/java/com/example/employee_management/mapper/EmployeeRowMapper.java
@@ -0,0 +1,25 @@
+package com.example.employee_management.mapper;
+
+import com.example.employee_management.entity.*;
+import org.springframework.jdbc.core.RowMapper;
+import java.util.*;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class EmployeeRowMapper implements RowMapper<Employee> {
+    @Override
+    public Employee mapRow(ResultSet rs, int rowNum) throws SQLException {
+        Employee employee = new Employee();
+        employee.setName(rs.getString("EmployeeName"));
+        employee.setSalary(rs.getInt("Salary"));
+        employee.setDateOfBirth(rs.getString("DateOfBirth"));
+        Contact contact = new Contact();
+        contact.setEmail(rs.getString("Email"));
+        contact.setPhoneNumber(rs.getString("PhoneNumber"));
+        List<Contact> contacts = new ArrayList<>();
+        contacts.add(contact);
+        employee.setContacts(contacts);
+
+        return employee;
+    }
+}
