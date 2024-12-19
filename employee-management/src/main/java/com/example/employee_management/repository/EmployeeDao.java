7d309ef4b9433b8e56bb36057a7d0239b5cec617
code Blooded <108016919+Ahmed-Naseer-01@users.noreply.github.com>
2024-12-19 16:28:12 +0500

    fix : introduce logger & format queries

--- employee-management/src/main/java/com/example/employee_management/repository/EmployeeDao.java
+++ employee-management/src/main/java/com/example/employee_management/repository/EmployeeDao.java
@@ -3,21 +3,27 @@ package com.example.employee_management.repository;
 import com.example.employee_management.entity.*;
 import com.example.employee_management.mapper.EmployeeRowMapper;
 import jakarta.transaction.Transactional;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
 import java.sql.*;
 import java.util.List;
-@Repository
-public class EmployeeDao {
+import java.util.Optional;
 
+@Repository
+public class EmployeeDao implements DAO<Employee> {
+    private static final Logger log = LoggerFactory.getLogger(EmployeeDao.class);
     @Autowired
     private JdbcTemplate jdbcTemplate;
 
+    // add Employee
     @Transactional
-    public boolean insertEmployee(Employee employee) {
+    public boolean add(Employee employee) {
         boolean status = false;
         try {
             // Insert the Employee and retrieve generated id
@@ -47,50 +53,73 @@ public class EmployeeDao {
 
                     // If any contact insertion fails, rollback
                     if (contactRowsAffected <= 0) {
+                        log.info("Failed to add contact to database");
                         return false; // Rollback happens here automatically due to @Transactional
                     }
                 }
+                log.info("Successfully added contact to database");
                 status = true;
             }
         } catch (Exception e) {
             e.printStackTrace();
             status = false;
         }
+        log.info("Successfully added employee to database");
         return status;
     }
 
     // Delete Employee by ID
-    public boolean deleteEmployeeById(int id) {
+    public boolean delete(int id) {
         String sql = "DELETE FROM Employee WHERE id = ?";
         int rowsAffected = jdbcTemplate.update(sql, id);
         return rowsAffected > 0;
     }
 
-    // Find Employee by ID
-    public Employee findEmployeeById(int id) {
-        String sql = "SELECT e.name AS EmployeeName, " +
-                "e.salary AS Salary, " +
-                "e.dateOfBirth AS DateOfBirth, " +
-                "c.email AS Email, " +
-                "c.phoneNumber AS PhoneNumber " +
-                "FROM Employee e " +
-                "LEFT JOIN Contact c ON e.id = c.employee_id " +
-                "WHERE e.id = ?";
 
-        return jdbcTemplate.queryForObject(sql, new Object[]{id}, new EmployeeRowMapper());
+    // Find Employee by ID
+    public Optional<Employee> getById(int id) {
+        String sql = "SELECT \n" +
+                "    e.id AS EmployeeID,\n" +
+                "  e.name AS EmployeeName, \n" +
+                "  e.salary AS Salary, \n" +
+                "  e.dateOfBirth AS DateOfBirth, \n" +
+                "  c.email AS Email, " +
+                "  c.phoneNumber AS PhoneNumber \n" +
+                "FROM Employee e \n" +
+                "LEFT JOIN Contact c \n" +
+                "  ON e.id = c.employee_id \n" +
+                "WHERE e.id = ? \n";
+
+        try {
+            Employee employee = jdbcTemplate.queryForObject(sql, new EmployeeRowMapper(), id);
+            return Optional.ofNullable(employee);
+        } catch (EmptyResultDataAccessException e) {
+        //  if employee is null
+            return Optional.empty();
+        }
     }
 
+
     // Get All Employees
-    public List<Employee> getAllEmployees() {
+    public List<Employee> getAll() {
         String sql = "SELECT \n" +
+                "    e.id AS EmployeeID,\n" +
                 "    e.name AS EmployeeName,\n" +
                 "    e.salary AS Salary,\n" +
                 "    e.dateOfBirth AS DateOfBirth,\n" +
                 "    c.email AS Email,\n" +
-                "    c.phoneNumber as PhoneNumber FROM Employee e" +
+                "    c.phoneNumber AS PhoneNumber\n" +
+                "FROM Employee e\n" +
                 "LEFT JOIN Contact c \n" +
-                "    ON e.id = c.employee_id";
+                "    ON e.id = c.employee_id;\n";
         return jdbcTemplate.query(sql, new EmployeeRowMapper());
+
+    }
+
+    // update employee
+    public void update(int id, Employee employee) {
+
+
 
     }
 }
