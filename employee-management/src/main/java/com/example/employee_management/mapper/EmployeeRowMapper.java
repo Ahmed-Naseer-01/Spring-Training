152b0ce8879877dbba8d42d4d2d00266b2a2aacc
code Blooded <108016919+Ahmed-Naseer-01@users.noreply.github.com>
2024-12-19 16:26:05 +0500

    fix : add emp id field

--- employee-management/src/main/java/com/example/employee_management/mapper/EmployeeRowMapper.java
+++ employee-management/src/main/java/com/example/employee_management/mapper/EmployeeRowMapper.java
@@ -10,16 +10,20 @@ public class EmployeeRowMapper implements RowMapper<Employee> {
     @Override
     public Employee mapRow(ResultSet rs, int rowNum) throws SQLException {
         Employee employee = new Employee();
+        employee.setId(rs.getInt("EmployeeID"));
         employee.setName(rs.getString("EmployeeName"));
         employee.setSalary(rs.getInt("Salary"));
         employee.setDateOfBirth(rs.getString("DateOfBirth"));
+
         Contact contact = new Contact();
         contact.setEmail(rs.getString("Email"));
         contact.setPhoneNumber(rs.getString("PhoneNumber"));
+
         List<Contact> contacts = new ArrayList<>();
         contacts.add(contact);
         employee.setContacts(contacts);
 
         return employee;
     }
+
 }
