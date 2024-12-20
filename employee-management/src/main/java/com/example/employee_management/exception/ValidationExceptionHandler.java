444225423abbddf0de32da6a6d1bfcf9effca5b7
code Blooded <108016919+Ahmed-Naseer-01@users.noreply.github.com>
2024-12-20 12:39:52 +0500

    add : add feat global ValidationExceptionHandler

---
+++ employee-management/src/main/java/com/example/employee_management/exception/ValidationExceptionHandler.java
@@ -0,0 +1,23 @@
+package com.example.employee_management.exception;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+// define global exception
+@ControllerAdvice
+public class ValidationExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getFieldErrors().forEach(error ->
+                errors.put(error.getField(), error.getDefaultMessage())
+        );
+        return ResponseEntity.badRequest().body(errors);
+    }
+}
