2c1e6e8f2ee16e8429c2299bc923b1b0ecc6ac6d
code Blooded <108016919+Ahmed-Naseer-01@users.noreply.github.com>
2024-12-27 15:53:25 +0500

    feat : add EmployeeService

---
+++ expense_reimbursement_system/src/main/java/com/example/expense_reimbursement_system/service/EmployeeService.java
@@ -0,0 +1,35 @@
+package com.example.expense_reimbursement_system.service;
+
+
+import com.example.expense_reimbursement_system.entity.Employee;
+import com.example.expense_reimbursement_system.entity.Role;
+import com.example.expense_reimbursement_system.repository.EmployeeRepository;
+import com.example.expense_reimbursement_system.repository.RoleRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Service
+public class EmployeeService {
+    private final EmployeeRepository employeeRepository;
+
+    public EmployeeService(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
+
+    @Transactional
+    public Employee createEmployee(Employee employee) {
+        try {
+            return employeeRepository.save(employee);
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new RuntimeException("Failed to save the employee: " + e.getMessage(), e);
+        }
+    }
+
+    public List<Employee> getAllEmployees() {
+        return employeeRepository.findAll();
+    }
+}
