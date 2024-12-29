c5eb740f4a8fe5245c7a5f3de49c023254b80973
code Blooded <108016919+Ahmed-Naseer-01@users.noreply.github.com>
2024-12-29 17:49:49 +0500

    feat(entity) : add CategoryPackage

---
+++ expense_reimbursement_system/src/main/java/com/example/expense_reimbursement_system/entity/CategoryPackage.java
@@ -0,0 +1,56 @@
+package com.example.expense_reimbursement_system.entity;
+
+import com.example.expense_reimbursement_system.entity.Categories;
+import jakarta.persistence.*;
+
+@Entity
+public class CategoryPackage {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id", nullable = false)
+    private Categories category;
+
+    @Column(nullable = false, length = 50)
+    private String packageName;
+
+    @Column(nullable = false)
+    private int expenseLimit;
+
+    // default constructor
+    public CategoryPackage() {}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Categories getCategory() {
+        return category;
+    }
+
+    public void setCategory(Categories category) {
+        this.category = category;
+    }
+
+    public String getPackageName() {
+        return packageName;
+    }
+
+    public void setPackageName(String packageName) {
+        this.packageName = packageName;
+    }
+
+    public Integer getExpenseLimit() {
+        return expenseLimit;
+    }
+
+    public void setExpenseLimit(Integer expenseLimit) {
+        this.expenseLimit = expenseLimit;
+    }
+}
