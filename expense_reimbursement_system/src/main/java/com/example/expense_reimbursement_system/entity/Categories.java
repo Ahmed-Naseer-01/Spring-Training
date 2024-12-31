c17e1963a3eeecf1edd39214c5bcb58d9c328ce7
code Blooded <108016919+Ahmed-Naseer-01@users.noreply.github.com>
2024-12-31 11:01:34 +0500

    fix : add validation annotation on fields

--- expense_reimbursement_system/src/main/java/com/example/expense_reimbursement_system/entity/Categories.java
+++ expense_reimbursement_system/src/main/java/com/example/expense_reimbursement_system/entity/Categories.java
@@ -10,7 +10,9 @@ public class Categories {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(nullable = false, unique = true)
     private String name;
+    @Column(nullable = false)
     private boolean status;
     @OneToMany(mappedBy = "category", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     private List<Expense> expenses;
