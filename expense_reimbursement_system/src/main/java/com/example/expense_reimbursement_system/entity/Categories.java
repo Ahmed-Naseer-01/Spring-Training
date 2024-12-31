1e79054a5798afe6245abed2d35f18fd0f4b68c7
code Blooded <108016919+Ahmed-Naseer-01@users.noreply.github.com>
2024-12-31 14:34:31 +0500

    fix : fix the datatype of id (PK)

--- expense_reimbursement_system/src/main/java/com/example/expense_reimbursement_system/entity/Categories.java
+++ expense_reimbursement_system/src/main/java/com/example/expense_reimbursement_system/entity/Categories.java
@@ -1,7 +1,6 @@
 package com.example.expense_reimbursement_system.entity;
 
 import jakarta.persistence.*;
-
 import java.util.List;
 
 
@@ -9,7 +8,7 @@ import java.util.List;
 public class Categories {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private int id;
     @Column(nullable = false, unique = true)
     private String name;
     @Column(nullable = false)
@@ -17,10 +16,24 @@ public class Categories {
     @OneToMany(mappedBy = "category", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     private List<Expense> expenses;
 
-
     public Categories() {}
+    public int getId() {
+        return id;
+    }
 
-    public void setId(Long id) {
+    public String getName() {
+        return name;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public List<Expense> getExpenses() {
+        return expenses;
+    }
+
+    public void setId(int id) {
         this.id = id;
     }
 
@@ -32,22 +45,8 @@ public class Categories {
         this.status = status;
     }
 
-    public Long getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public List<Expense> getExpenses() {
-        return expenses;
-    }
-
     public void setExpenses(List<Expense> expenses) {
         this.expenses = expenses;
     }
-    public boolean isStatus() {
-        return status;
-    }
+
 }
