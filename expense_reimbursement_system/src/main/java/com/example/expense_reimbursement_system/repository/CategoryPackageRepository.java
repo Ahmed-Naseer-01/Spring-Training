4a2685e480e2269f40eeb2b469e1e4ee2b39f0c8
code Blooded <108016919+Ahmed-Naseer-01@users.noreply.github.com>
2025-01-01 14:52:49 +0500

    feat: added new feature for calculating amount by category

--- expense_reimbursement_system/src/main/java/com/example/expense_reimbursement_system/repository/CategoryPackageRepository.java
+++ expense_reimbursement_system/src/main/java/com/example/expense_reimbursement_system/repository/CategoryPackageRepository.java
@@ -3,9 +3,18 @@ package com.example.expense_reimbursement_system.repository;
 
 import com.example.expense_reimbursement_system.entity.CategoryPackage;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface CategoryPackageRepository extends JpaRepository<CategoryPackage, Integer> {
-    public CategoryPackage findByPackageName(String packageName);
+    CategoryPackage findByPackageName(String packageName);
+
+    @Query("SELECT cp.expenseLimit FROM CategoryPackage cp " +
+            "JOIN RoleCategoryPackage rcp ON cp.id = rcp.categoryPackage.id " +
+            "WHERE rcp.role.id = :roleId AND cp.category.id = :categoryId")
+    int findExpenseLimitByRoleAndCategory(@Param("roleId") int roleId, @Param("categoryId") int categoryId);
 }
